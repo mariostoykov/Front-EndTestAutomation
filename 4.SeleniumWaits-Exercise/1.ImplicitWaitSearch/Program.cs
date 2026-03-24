3568ffddaa5d727581952df92a1b67fd8c495588
mariostoykov <mario.stoykov12@gmail.com>
2026-03-24 18:38:54 +0200

    Add solution for 4.SeleniumWaits-Exercise

---
+++ 4.SeleniumWaits-Exercise/1.ImplicitWaitSearch/Program.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SearchWithImplicitWait
+{
+    public class ImplicitWaitSearchProduct
+    {
+        [TestFixture]
+        public class ImplicitWaitTests
+        {
+            IWebDriver driver;
+
+            [SetUp]
+            public void Setup()
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver.Url = "http://practice.bpbonline.com/";
+            }
+
+            [Test, Order(1)]
+            public void SearchProduct_Keyboard_ShouldAddToCart()
+            {
+                // Fill in the search field textbox
+                driver.FindElement(By.Name("keywords")).SendKeys("keyboard");
+
+                // Click on the search icon
+                driver.FindElement(By.XPath("//input[@title=' Quick Find ']")).Click();
+
+                try
+                {
+                    // Click on Buy Now link
+                    driver.FindElement(By.LinkText("Buy Now")).Click();
+
+                    // Verify text
+                    Assert.That(driver.PageSource.Contains("keyboard"),
+                        "The product 'keyboard' was not found in the cart page.");
+
+                    Console.WriteLine("Scenario Completed");
+                }
+
+                catch (Exception ex)
+                {
+                    Assert.Fail("Unexpected exception" +  ex.Message);
+                }
+            }
+
+            [Test, Order(2)]
+            public void SearchProduct_Junk_ShouldThrowNoSuchElementException()
+            {
+                // Fill in the search field textbox
+                driver.FindElement(By.Name("keywords")).SendKeys("junk");
+
+                // Click on the search icon
+                driver.FindElement(By.XPath("//input[@title=' Quick Find ']")).Click();
+
+                try
+                {
+                    // Try to click on Buy Now link
+                    driver.FindElement(By.LinkText("Buy Now")).Click();
+                }
+                catch (NoSuchElementException ex)
+                {
+                    // Verify the exception for non-existing product
+                    Assert.Pass("Expected NoSuchElementException was thrown.");
+                    Console.Write("Timeout - " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("Unexpected exception: " + ex.Message);
+                }
+
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                driver.Close();
+            }
+        }
+    }
+}
